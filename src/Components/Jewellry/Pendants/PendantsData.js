5751a25dc5d10135e771c56a92bc4f113fb832c1
Parv Bharti <parvbarti2@gmail.com>
2021-05-15 23:06:22 +0530

    added component

---
+++ src/Components/Jewellry/Pendants/PendantsData.js
@@ -0,0 +1,69 @@
+function rer(val){
+    return require(`../../../Images/pendants/${val}`)
+}
+export const data=
+[
+    {
+        "productId":"P01",
+        "url":rer("pn1.jpg"),
+        "price":"299",
+        "desc":"oxidised",
+        "heading":"Pendant01"
+    },
+    {
+        "productId":"P02",
+        "url":rer("pn2.jpg"),
+        "price":"799",
+        "desc":"Non oxidised",
+        "heading":"Pendant02" 
+    },
+    {
+        "productId":"P03",
+        "url":rer("pn3.jpg"),
+        "price":"1099",
+        "desc":"Non oxidised",
+        "heading":"Pendant03" 
+    },
+    {
+        "productId":"P04",
+        "url":rer("pn4.jpg"),
+        "price":"499",
+        "desc":"oxidised",
+        "heading":"Pendant04" 
+    },
+    {
+        "productId":"P05",
+        "url":rer("pn5.jpg"),
+        "price":"699",
+        "desc":"Non oxidised",
+        "heading":"Pendant05" 
+    },
+    {
+        "productId":"P06",
+        "url":rer("pn6.jpg"),
+        "price":"799",
+        "desc":"oxidised",
+        "heading":"Pendant06" 
+    },
+    {
+        "productId":"P07",
+        "url":rer("pn7.jpg"),
+        "price":"799",
+        "desc":"Non oxidised",
+        "heading":"Pendant07" 
+    },
+    {
+        "productId":"P08",
+        "url":rer("pn8.jpg"),
+        "price":"399",
+        "desc":"oxidised",
+        "heading":"Pendant08" 
+    },
+    {
+        "productId":"P09",
+        "url":rer("pn9.jpg"),
+        "price":"799",
+        "desc":"Non oxidised",
+        "heading":"Pendant09" 
+    }
+]
